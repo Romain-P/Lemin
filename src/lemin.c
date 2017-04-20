0cac4cfedca985e7f0ce53fb9b2aa1d33016542b
Raphaël Goulmot <raphael.goulmot@epitech.eu>
2017-04-20 22:49:44 +0200

    Ajout des ficheirs manquant

---
+++ src/lemin.c
@@ -0,0 +1,113 @@
+/*
+** lemin.c for lemin in /home/raphael.goulmot/rendu/CPE_2016_Lemin
+** 
+** Made by Raphaël Goulmot
+** Login   <raphael.goulmot@epitech.net>
+** 
+** Started on  Thu Apr 20 20:43:48 2017 Raphaël Goulmot
+** Last update Thu Apr 20 22:48:29 2017 Raphaël Goulmot
+*/
+
+#include "util.h"
+#include "lemin.h"
+#include <stdbool.h>
+
+static char	possible_path(t_path *path, t_data *world)
+{
+  t_elem        *elem;
+  t_crosser	*crosser;
+  int		i;
+  char		check;
+
+  elem = world->crossers->first;
+  while (elem)
+    {
+      crosser = (t_crosser *) elem->get;
+      if (!crosser->path || crosser->path->nodes[crosser->step] == world->end)
+	continue;
+      i = 0;
+      check = 1;
+      while (check && i < crosser->path->size - crosser->step - 1)
+	{
+	  if (path->size < i
+	      && crosser->path->nodes[crosser->step + i] == path->nodes[i + 1])
+	    check = 0;
+	  i++;
+	}
+      if (check)
+	return (1);
+      elem = elem->next;
+    }
+  return (0);
+}
+
+static void	init_path(t_crosser *crosser, t_data *world)
+{
+  t_elem	*elem;
+  int		i;
+
+  elem = world->paths->first;
+  while (elem)
+    {
+      if (possible_path((t_path *) elem->get, world))
+	{
+	  crosser->path = (t_path *) elem->get;
+	  crosser->step = 1;
+	  return;
+	}
+      elem = elem->next;
+    }
+}
+
+static void	init_crossers(t_data *world)
+{
+  t_elem	*elem;
+
+  elem = world->crossers->first;
+  while (elem)
+    {
+      if (!((t_crosser *) elem->get))
+	init_path((t_crosser *) elem->get, world);
+      elem = elem->next;
+    }
+}
+
+static char	end_crossers(t_data *world)
+{
+  t_elem        *elem;
+  t_crosser	*crosser;
+
+  elem = world->crossers->first;
+  while (elem)
+    {
+      if (!(crosser = (t_crosser *) elem->get)
+	  || crosser->path->nodes[crosser->step] != world->end)
+	return (0);
+      elem = elem->next;
+    }
+  return (1);
+}
+
+void	launch_lemin(t_data *world)
+{
+  t_elem	*elem;
+  t_crosser	*crosser;
+
+  while (!end_crossers(world))
+    {
+      init_crossers(world);
+      elem = world->crossers->first;
+      while (elem)
+	{
+	  crosser = (t_crosser *) elem->get;
+	  if (crosser->path && crosser->path->nodes[crosser->step] != world->end)
+	    {
+	      crosser->step++;
+	      display_format("P%d-%s", crosser->id
+			     , crosser->path->nodes[crosser->step]->name);
+	    }
+	  elem = elem->next;
+	}
+      display_char('\n', false);
+    }
+}
