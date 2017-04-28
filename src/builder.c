19a1077dcc6f7ba4b8d8981889e4546959c0be00
Romain <romain.pillot@epitech.eu>
2017-04-28 14:41:03 +0000

    Fixing parser for retarded mouli

--- src/builder.c
+++ src/builder.c
@@ -5,7 +5,7 @@
 ** Login   <romain pillot@epitech.eu>
 ** 
 ** Started on  Fri Apr 21 00:35:40 2017 romain pillot
-** Last update Fri Apr 28 14:38:43 2017 romain pillot
+** Last update Fri Apr 28 14:39:22 2017 romain pillot
 */
 
 #include <stdlib.h>
@@ -127,11 +127,10 @@ bool		build_link(t_data *data, char *str)
   len = tab_length(split);
   node_a = len == 2 ? find_node(data->nodes, split[0], true) : NULL;
   node_b = len == 2 ? find_node(data->nodes, split[1], true) : NULL;
-  if (len == 2 && node_a && node_b &&
-      find_node(node_a->nodes, node_b->label, false))
-    fdisplay_format("warning: ignored link '%s' (already built).\n", str);
-  else if (len == 2 && node_a && node_b)
+  if (len == 2 && node_a && node_b)
     {
+      if (find_node(node_a->nodes, node_b->label, false))
+	fdisplay_format("warning: link '%s' (already built).\n", str);
       if (!(link = malloc(sizeof(t_link))))
 	return (false);
       list_add(data->links, link);
